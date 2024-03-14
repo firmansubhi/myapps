e288b121301a1531478046474073298495deb71d
unknown <firmansubhi@gmail.com>
2024-03-14 09:45:24 +0700

    next commit bla bla bla

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { View } from "react-native";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import { AuthProvider, useAuth } from "./src/context/AuthContext";
